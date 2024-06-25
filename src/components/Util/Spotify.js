730e93f48a8644e04c2210b56d784a98d75b37c3
m1moraru <164013436+m1moraru@users.noreply.github.com>
2024-06-25 05:38:26 +0100

    Update Spotify.js

--- src/components/Util/Spotify.js
+++ src/components/Util/Spotify.js
@@ -1,5 +1,5 @@
 const clientId = '84cc5220d8524668b0458659202442c8'; 
-const redirectUri = process.env.REACT_APP_REDIRECT_URI || 'https://m1moraru.github.io/jammming1/'
+const redirectUri = 'http://localhost:3000/callback'
 let accessToken;
 
 const Spotify = {
